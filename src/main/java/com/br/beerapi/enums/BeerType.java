4f625cbfe8ffa18d7433d15b87ab7eea1828579e
Takeo <oswaldotakeo1@gmail.com>
2022-05-31 20:48:44 -0300

    Creating model and enum

---
+++ src/main/java/com/br/beerapi/enums/BeerType.java
@@ -0,0 +1,23 @@
+package com.br.beerapi.enums;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
+public enum BeerType {
+
+    LAGER("Lager"),
+    MALZBIER("Malzbier"),
+    WITBIER("Witbier"),
+    WEISS("Weiss"),
+    ALE("Ale"),
+    IPA("Ipa"),
+    STOUT("Stout");
+
+
+    private final String description;
+
+}
